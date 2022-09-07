065395aebc4072657aa8040d1e426397fddf7187
YuriOlivs <yuri.os.2004@outlook.com>
2022-09-07 16:43:45 -0300

    criando menu responsivo

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const navBar = document.getElementById("navbar"), 
     navList = document.getElementById("navList"),
     navListPosition = navList.getAttribute("left"),
+    line1 = document.getElementById("line1"),
+    line2 = document.getElementById("line2"),
+    line3 = document.getElementById("line3"),
     mobileMenu = document.getElementById("mobileMenu"),
     verMais = document.getElementById("verMais"),
     target = document.querySelectorAll("[data-anime]");
@@ -23,9 +26,16 @@ const ativarScroll = () => {
 const mobileNavbar = () => {
     if(navListPosition == 0) {
         navList.classList.remove("nav-list-active")
+        line1.classList.remove("line1-active")
+        line2.classList.remove("line2-active")
+        line3.classList.remove("line3-active")
     }  else {
         navList.classList.toggle("nav-list-active")
+        line1.classList.toggle("line1-active")
+        line2.classList.toggle("line2-active")
+        line3.classList.toggle("line3-active")
     }
+
 }
 
 window.addEventListener("scroll", ativarScroll)
